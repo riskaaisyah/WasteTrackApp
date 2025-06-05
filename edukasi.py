6cc829096ecf96a0eff19dd259ec372c3b3e03aa
riskaaisyah <130236366+riskaaisyah@users.noreply.github.com>
2025-06-05 15:44:37 +0700

    Add files via upload

---
+++ edukasi.py
@@ -0,0 +1,32 @@
+waste_info = {
+    "plastic": {
+        "kategori": "Anorganik",
+        "daur_ulang": "Bisa, melalui bank sampah atau industri daur ulang plastik.",
+        "edukasi": "Kurangi penggunaan plastik sekali pakai. Gunakan botol atau tas belanja ulang."
+    },
+    "metal": {
+        "kategori": "Anorganik",
+        "daur_ulang": "Bisa, sangat bernilai di industri daur ulang.",
+        "edukasi": "Pisahkan kaleng/metal dari sampah organik agar mudah diproses."
+    },
+    "glass": {
+        "kategori": "Anorganik",
+        "daur_ulang": "Bisa, terutama kaca bening.",
+        "edukasi": "Jangan pecahkan kaca sembarangan. Bisa didaur ulang jadi botol atau kerajinan."
+    },
+    "paper": {
+        "kategori": "Organik",
+        "daur_ulang": "Bisa, terutama jika belum kotor atau berminyak.",
+        "edukasi": "Kumpulkan kertas bekas bersih. Bisa dibuat kerajinan atau kertas daur ulang."
+    },
+    "cardboard": {
+        "kategori": "Organik",
+        "daur_ulang": "Bisa, mudah diolah kembali.",
+        "edukasi": "Simpan di tempat kering agar tidak rusak."
+    },
+    "trash": {
+        "kategori": "Campuran / Tidak bisa didaur ulang",
+        "daur_ulang": "Tidak bisa",
+        "edukasi": "Usahakan memisahkan sampah sejak awal agar tidak menjadi sampah campuran."
+    }
+}
